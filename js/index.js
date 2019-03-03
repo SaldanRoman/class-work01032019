b85a3569e8eb499158a67fef028f2c6f2318bc39
SaldanRoman <34034422+SaldanRoman@users.noreply.github.com>
2019-03-03 02:02:50 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,22 @@
-for (let i = 100; i <= 1; i--) {
+/*for (let i = 100; i >= 1; i--) {
+    if(i%2==1){
     console.log(i);
-}
+    }
+    }*/
+/*let i =0;
+while(i<=100){
+    console.log(i);
+    i++;
+
+}*/
+/*let i = 0;
+do{
+    console.log(i); //перший раз умова не перевіряється
+} while(i<=100);    //коли ми не знаєм скільки разів викликати, коли взаємодієм з користувачем
+*/
+
+/*let num = parseFloat(prompt('Enter the number'));
+while(Number.isNaN(num)){
+    alert('Please enter the number');
+    num = parseFloat(prompt('Enter the number'));
+}*/
